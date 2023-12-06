db87d6baa6aaf64ec6de4458deb2cad259f7ba9e
Jean Lin <jeanlin@Jeans-MacBook-Pro.local>
2023-12-06 13:38:57 -0800

    “change”

--- js/my.js
+++ js/my.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
         AOS.init();
     })
 
-
     //tab
     $(".uk-tab li").on("click", function () {
         $(".slider").slick('refresh');
@@ -19,18 +18,13 @@ $(document).ready(function () {
     $('.main-nav ul.anchor li a').click(function () {
         $('#header,#toggle, .main-nav').removeClass('on')
     })
+
     //錨點捲動
     $('a[href*="#sec_"]').on('click', function () {
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top
         }, 500, 'linear');
     })
-
-    //一按鈕觸發另一按鈕
-    $('#anchor-qa3').on('click', function () {
-        $('#qa3').click();
-    })
-
 
     //gotop        
     var float_btn = $('.float_btn');
@@ -48,7 +42,6 @@ $(document).ready(function () {
         }
     });
     if ($(window).width() < 769) {
-
         $(window).scroll(function () {
             //最後一頁scrollTop=body-window，50是預留空間
             last = $("body").height() - $(window).height() - 200
@@ -64,33 +57,33 @@ $(document).ready(function () {
 
 
     //slick
-    $(".slider").slick({
-        autoplaySpeed: 1500,
-        arrows: false,
-        autoplay: true,
-        dots: true,
-        infinite: true,
-        // centerMode: true,
-        centerPadding: '0px',
-        slidesToShow: 4,
-        slidesToScroll: 2,
-        responsive: [
-            {
-                breakpoint: 1040,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 786,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            },
-        ],
+    // $(".slider").slick({
+    //     autoplaySpeed: 1500,
+    //     arrows: false,
+    //     autoplay: true,
+    //     dots: true,
+    //     infinite: true,
+    //     // centerMode: true,
+    //     centerPadding: '0px',
+    //     slidesToShow: 4,
+    //     slidesToScroll: 2,
+    //     responsive: [
+    //         {
+    //             breakpoint: 1040,
+    //             settings: {
+    //                 slidesToShow: 2,
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 786,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1,
+    //             }
+    //         },
+    //     ],
 
-    });
+    // });
 
 
 
